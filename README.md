bc7da187ae058a5b52efdbbb81fcf681edce080c
peterson <peterson@peterson-desktop>
2017-12-21 14:29:37 -0200

    Acrescetadas algumas instruções de uso no readme

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# cms
+# base cms
 # 
-Um cms feito no fw codeigniter codeigniter com painel administrativo admilLTE, está em fase de construção.
-sera usado pela equipe FroceTeam como base para a criação do superCMS
+Esse projeto serve como base de programação para criar projetos em php, as funções que ele ja tem são:
+-- sistema de login no painel adm pronto
+---- endereço base_url/admin
+---- login admin
+---- senha 123
+-- cadastro de noticias (posts) com categorias e tags, 
+-- classe para receber mensagens de forms do site, guardar no banco e enviar por email
+-- ja esta integrado com painel adminLTE
+-- classe option onde pode ser guardado varios tipos de informação tipo links de redes sociais, dados de empresa, dados de configuração,
+para serem editaveis pelo painel e exibidos no site com facilidade
+-- classe para armazer logs no bd de tudo que acontecer de errado no php
+-- classe para contar visualizações, cliques ou o que for necessario implementar no projeto
+-- linguagem pt-br para as mensagens do sistema do codeigniter incluida e ativa
+
+configurações para funcionar em localhost
+--configurar a base url no application/config/config:26
+--trocar a encryption key no application/config/config:327 (recomendado)
+--configurar os dados dos bancos no model connect (o $con é chamado pela função estática desse model)
+
+obs.:
+-- Todos os models criados na pasta models serão carregados no autoload nativo do codeigniter por meio de uma gambiarra que
+fiz no arquivo aplication/config/autoload
+-- o model Helper é onde eu guardo todas as funções uteis, depois vamos tranforma-lo em um helper mesmo conforme o padrao do codeigniter
--- application/models/Conect_model.php
+++ application/models/Conect_model.php
@@ -1,55 +1,57 @@
 <?php
 
 /* codigo que impossibilita o acesso direto sem passar pela home */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH')	OR	exit('No direct script access allowed');
 /* todo controller DEVE extender CI_Controller */
 
-class Conect_model extends CI_Model {
-	/* construtor da classe que carregar os principais helpers
-	  que podem ser usados dentro de toda a classe */
+class	Conect_model	extends	CI_Model	{
+		/* construtor da classe que carregar os principais helpers
+			 que podem ser usados dentro de toda a classe */
 
-	function __construct() {
-		/* contrutor da classe pai */
-		parent::__construct();
-		/* abaixo deverão ser carregados helpers, libraries e models utilizados
-		  por este model */
-	}
-
-	public static function conectar() {
-		$con = Conect_model::gerarPDO();
-		return $con;
-	}
-
-	private static function gerarPDO() {
-		if (ENVIRONMENT === 'development') {
-			//BANCO DE AMBIENTE LOCAL ##########
-			
-			//endereço do host
-			$host = "localhost";
-			//nome do banco de dados
-			$banco = "cms";
-			//login
-			$login = "root";
-			//senha
-			$senha = "";
-			//criando a conexão
-			$con = new PDO("mysql:host=$host;dbname=$banco", "$login", "$senha");
-			return $con;
-		} else {
-			//BANCO DE AMBIENTE PRODUÇÃO ##########
-			
-			//endereço do host
-			$host = "localhost";
-			//nome do banco de dados
-			$banco = "buscasma_site";
-			//login
-			$login = "buscasma_user";
-			//senha
-			$senha = "w2Gt0hsRMt]l";
-			//criando a conexão
-			$con = new PDO("mysql:host=$host;dbname=$banco", "$login", "$senha");
-			return $con;
+		function	__construct()	{
+				/* contrutor da classe pai */
+				parent::__construct();
+				/* abaixo deverão ser carregados helpers, libraries e models utilizados
+					 por este model */
 		}
-	}
+
+		public	static	function	conectar()	{
+				$con	=	Conect_model::gerarPDO();
+				return	$con;
+		}
+
+		private	static	function	gerarPDO()	{
+				//verificando se é localhost ou produção
+				$string	=	base_url();	//string a ser analisada
+				$palavra	=	'localhost';	//palavra a ser buscada no meio da string
+				$regex	=	'/'	.	$palavra	.	'/';	//formando a regex para ser buscada
+				if	(preg_match($regex,	$string))	{
+						//BANCO DE AMBIENTE LOCAL ##########
+						//endereço do host
+						$host	=	"localhost";
+						//nome do banco de dados
+						$banco	=	"cms";
+						//login
+						$login	=	"root";
+						//senha
+						$senha	=	"";
+						//criando a conexão
+						$con	=	new	PDO("mysql:host=$host;dbname=$banco",	"$login",	"$senha");
+						return	$con;
+				}		else	{
+						//BANCO DE AMBIENTE PRODUÇÃO ##########
+						//endereço do host
+						$host	=	"";
+						//nome do banco de dados
+						$banco	=	"";
+						//login
+						$login	=	"";
+						//senha
+						$senha	=	"";
+						//criando a conexão
+						$con	=	new	PDO("mysql:host=$host;dbname=$banco",	"$login",	"$senha");
+						return	$con;
+				}
+		}
 
 }
